7796a8c78ce4fd1912d4f034d30738ae2fa9c206
vierge <marancholia@gmail.com>
2020-06-07 22:10:48 -0400

    added routes for most pages. still need to filter and build controllers.

---
+++ glaring-api/app/controllers/api_controller.rb
@@ -0,0 +1,5 @@
+class ApiController < ApplicationController
+  def get
+    put "welcome to the Glaring API. do you need help?"
+  end
+end
--- glaring-api/config/routes.rb
+++ glaring-api/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'api#index'
+
+  scope path: '/api', controller: :api do
+  
+    resources :users, only: [:show, :create, :destroy]
+
+    resources :groups do
+      resources :events 
+    end
+  end
 end
