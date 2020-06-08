23c9e4750ec9c3a3c294cf0c1b816d075abddcfe
David <david.he07@yahoo.ca>
2020-06-08 18:15:51 -0400

    included code for sidebar and created components directory with Application.js and Application.scss within it

---
+++ glaring-client/src/components/Application.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Application.scss";
+
+export default function Application() {
+  return (
+    <div className="sidebar">
+      <div className="page">
+        <nav className="sidebar__menu">Event Scheduler</nav>
+      </div>
+    </div>
+  );
+}
