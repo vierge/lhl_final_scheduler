c37670fa68b8d414d4403200fd4c24367713a116
David <david.he07@yahoo.ca>
2020-06-08 22:52:13 -0400

    added code for GET request from api to client

--- glaring-client/src/components/Application.jsx
+++ glaring-client/src/components/Application.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Application.scss";
+import axios from "axios";
+
 import Sidebar from "./Sidebar/Index";
 
+
 export default function Application() {
+
+const [state, setState] = useState("smoko");
+  const [groups, setGroups] = useState([]);
+
+useEffect(() => {
+    axios.get(`/api/groups`)
+      .then((response) => {
+
+        console.log("AAA", response)
+        setGroups(()=> response.data);
+      });
+  }, [state])
+   
+
+
+
   return <Sidebar />;
 }
