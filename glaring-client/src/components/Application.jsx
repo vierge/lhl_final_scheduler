5fb1b85c3af919f6896c4fda4d0c7a2b9ba9d5df
David <david.he07@yahoo.ca>
2020-06-10 12:36:04 -0400

    added code in Application.jsx to render the GroupListItem onto the sidebar

--- glaring-client/src/components/Application.jsx
+++ glaring-client/src/components/Application.jsx
@@ -1,30 +1,41 @@
-import React from "react";
-import useAppData from "../hooks/useAppData";
+import React, {useState, useEffect} from "react";
+// import useAppData from "../hooks/useAppData";
 
 import "./Application.scss";
-import Testbed from "./Testbed.jsx";
+import axios from "axios"
+// import Testbed from "./Testbed.jsx";
 import Sidebar from "./Sidebar/Index";
+import GroupList from "./GroupList.js";
+
 
 export default function Application() {
-  const { state, setGroupData } = useAppData();
+  // const { state, setGroupData } = useAppData();
 
   const [state, setState] = useState("smoko");
   const [groups, setGroups] = useState([]);
 
-  // const groups = [
-  //   {
-  //     id: 1,
-  //     name: "Group 1",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Group 2",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Group 3",
-  //   },
-  // ];
+  const groupslisting = [
+    {
+      id: 1,
+      name: "Group 1",
+    },
+    {
+      id: 2,
+      name: "Group 2",
+    },
+    {
+      id: 3,
+      name: "Group 3",
+    },
+     {
+      id: 4,
+      name: "Group 4",
+    },
+     {
+      id: 5,
+      name: "Group 5",
+    },
+  ];
 
   useEffect(() => {
     axios.get(`/api/groups`).then((response) => {
@@ -34,14 +45,17 @@ export default function Application() {
   });
 
   return (
-    <body>
-      <Sidebar />
-      {/* <Testbed
-        data-cy="db-response"
-        users={state.users}
-        groups={state.groups}
-        setGroupData={setGroupData}
-      /> */}
-    </body>
+     <section className="sidebar">
+       <nav classname = "sidebar__menu">
+        Event Scheduler
+        <GroupList
+          groups={groupslisting}
+          group={"Group 1"}
+          setGroup={group => console.log(group)}
+        />
+      </nav>
+    </section>
+     
+     
   );
 }
