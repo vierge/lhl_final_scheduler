6c7e4282185cecbe5ef9431462c9fd21a43d79b0
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-06-10 19:45:16 -0400

    styled top portion of event component. shows img, event title, event description, and time

--- glaring-client/src/components/Event/Index.js
+++ glaring-client/src/components/Event/Index.js
@@ -8,8 +8,11 @@ export default function Event() {
   return (
     <main className="event">
       <img className="event__image" src="https://www.sciencenews.org/wp-content/uploads/2019/07/071019_MT_poker-ai_feat.jpg" />
-      <div className="event__title">EVENT TITLE</div>
-      <div className="event__description">Event Descrtipion goes here</div>
+      <div className="event__content">
+        <div className="event__title">EVENT TITLE</div>
+        <div className="event__description">Event Descrtipion goes here</div>
+      </div>
+      <div className="event__TIME"><p>TI</p><p>ME</p></div>
     </main>
   )
 }
