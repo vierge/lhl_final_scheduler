16b2b8bc6415cead07704e7b8f4fac064a124c31
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-06-10 18:25:58 -0400

    styled bare bones skeleton of event component

--- glaring-client/src/components/Event/Index.js
+++ glaring-client/src/components/Event/Index.js
@@ -7,7 +7,7 @@ export default function Event() {
 
   return (
     <main className="event">
-      <img className="event__image"  />
+      <img className="event__image" src="https://www.sciencenews.org/wp-content/uploads/2019/07/071019_MT_poker-ai_feat.jpg" />
       <div className="event__title">EVENT TITLE</div>
       <div className="event__description">Event Descrtipion goes here</div>
     </main>
