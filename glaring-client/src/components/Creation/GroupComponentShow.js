b052630a31c338171eba2f2e5c95106994b95fa3
David <david.he07@yahoo.ca>
2020-06-10 17:54:39 -0400

    added code for group compoenent in GroupComponentShow file

--- glaring-client/src/components/Creation/GroupComponentShow.js
+++ glaring-client/src/components/Creation/GroupComponentShow.js
@@ -6,10 +6,9 @@ export default function GroupComponentShow(props) {
 return (
 <main className="creation__card creation__card--show">
   <section className="creation__card-left">
-    <h2 className="text--regular">An event goes here</h2>
-    <h3 className="text--regular">An event goes here</h3>
-    <h4 className="text--regular">An event goes here</h4>
-
+      <h2 className="text--regular">An event goes here</h2>
+      <h3 className="text--regular">An event goes here</h3>
+      <h4 className="text--regular">An event goes here</h4>
     <section className="interviewer">
 
     </section>
