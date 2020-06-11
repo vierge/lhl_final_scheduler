00a7d9fca60247897be63dd28c2898759648fe2f
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-06-10 20:23:38 -0400

    added all parts of event component

--- glaring-client/src/components/Event/Index.js
+++ glaring-client/src/components/Event/Index.js
@@ -6,13 +6,21 @@ export default function Event() {
 
 
   return (
+    <div className="mom">
     <main className="event">
+      <div className="event__top">
       <img className="event__image" src="https://www.sciencenews.org/wp-content/uploads/2019/07/071019_MT_poker-ai_feat.jpg" />
       <div className="event__content">
         <div className="event__title">EVENT TITLE</div>
         <div className="event__description">Event Descrtipion goes here</div>
       </div>
-      <div className="event__TIME"><p>TI</p><p>ME</p></div>
+      </div>
+      <div className="event__attend">
+        <div className="event__attend--list">123456789</div>
+        <div className="event__attend--reso">Y/N</div>
+      </div>
     </main>
+    <aside className="event__TIME"><p>TI</p><p>ME</p></aside>
+    </div>
   )
 }
