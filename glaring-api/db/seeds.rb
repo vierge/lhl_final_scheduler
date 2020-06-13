08dcae823df522d83b2c92824e0c6039a5ae18c1
David <david.he07@yahoo.ca>
2020-06-12 20:23:50 -0400

    added seeds to event, memberships, and reservations

--- glaring-api/db/seeds.rb
+++ glaring-api/db/seeds.rb
@@ -22,7 +22,6 @@ Group.create(name: "Molly's Ex Girlfriends", description: "Molly's ex girlfriend
 User.create(name: "Mara", password: "cringelife", email: "marancholia@gmail.com", phone_number: nil, avatar: nil, colour: "deeppink")
 User.create(name: "David", password: "cringelife", email: "david@gmail.com", phone_number: nil, avatar: nil, colour: "blue")
 User.create(name: "Wares", password: "cringelife", email: "wares@gmail.com", phone_number: nil, avatar: nil, colour: "teagreen")
-User.create(name: "gay", password: "cringelife", email: "lesbian@gmail.com", phone_number: nil, avatar: nil, colour: "deeppurple")
 User.create(name: "headpats_pls", password: "cringelife", email: "catgirl.uwu@gmail.com", phone_number: nil, avatar: nil, colour: "goldenrod")
 User.create(name: "no_cops", password: "cringelife", email: "acab@gmail.com", phone_number: nil, avatar: nil, colour: "red")
 User.create(name: "videogames", password: "cringelife", email: "gamers_are_valid@gmail.com", phone_number: nil, avatar: nil, colour:"brown")
@@ -32,13 +31,29 @@ User.create(name: "password", password: "cringelife", email: "user@gmail.com", p
 # events
 
 Event.create(name: "smoko", description: "go away. i have ten whole minutes for this and it's hot", location: "out front by the fire hydrant", start_time: nil, photo: nil, end_time: nil, group_id: 1, colour: "red")
+Event.create(name: "basketball", description: "lets play some pickup basketabll", location: "at the local gym", start_time: nil, photo: nil, end_time: nil, group_id: 2, colour: "blue")
+Event.create(name: "video games", description: "are you a professional gamer or a decent gamer?", location: "at the local e-sports center", start_time: nil, photo: nil, end_time: nil, group_id: 3, colour: "purple")
+Event.create(name: "baseball", description: "get ready to swing for the fences", location: "at the baseball diamond", start_time: nil, photo: nil, end_time: nil, group_id: 4, colour: "yellow")
+Event.create(name: "book reading", description: "make sure to read often, really helps people gain knowledge", location: "at the library", start_time: nil, photo: nil, end_time: nil, group_id: 4, colour: "green")
+
 
 # memberships and reservations
 
 Membership.create(user_id: 1, group_id: 2)
 Membership.create(user_id: 8, group_id: 2, admin: true)
 Membership.create(user_id: 5, group_id: 2)
+Membership.create(user_id: 7, group_id: 2)
+Membership.create(user_id: 6, group_id: 2)
+Membership.create(user_id: 2, group_id: 2)
+Membership.create(user_id: 3, group_id: 2)
+Membership.create(user_id: 4, group_id: 2)
+
 
 Reservation.create(user_id: 1, event_id: 1, going: true, creator: true)
 Reservation.create(user_id: 5, event_id: 1, going: true)
 Reservation.create(user_id: 8, event_id: 1, going: false)
+Reservation.create(user_id: 9, event_id: 1, going: false)
+Reservation.create(user_id: 2, event_id: 1, going: false)
+Reservation.create(user_id: 3, event_id: 1, going: false)
+Reservation.create(user_id: 4, event_id: 1, going: false)
+Reservation.create(user_id: 6, event_id: 1, going: false)
