0b6b61d3ba07edff8d2ce4654da66b2abce87d57
Wares Fazelyar <waresfazelyar@Waress-MacBook-Pro.local>
2020-06-13 16:20:14 -0400

    fixed form so that all inputs are registered and logged in console

--- glaring-client/src/components/Event/EventForm.jsx
+++ glaring-client/src/components/Event/EventForm.jsx
@@ -12,32 +12,36 @@ export default function EventForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mom">
-        <main className="eventform">
+    <form className="eventform" onSubmit={handleSubmit(onSubmit)}>
+      {/* <div className="mom"> */}
+        {/* <main className="eventform"> */}
           <div className="eventform__top">
             {/* event image */}
             {/* <label for="img">Select image:</label>
             <input type="file" id="img" name="img" accept="image/*" /> */}
 
-            <input className="image" type="url" placeholder="Paste event image url" />
+            <input className="image" type="url" name = "image" placeholder="Paste event image url" ref={register} />
 
             <div className="eventform__content">
               <input type="text" name="title" placeholder="Enter event title" ref={register} />
               <input type="text" name="description" placeholder="Enter event description" ref={register} />
             </div>
             
-            <input type="submit"/>
+            
 
           </div>
-        </main>
+        {/* </main> */}
 
         <aside>
-          <input type="date" ref={register}/>
-          <input type="time" ref={register}/> 
+          <input type="date" name="date" ref={register} />
+          <input type="time" name="time" ref={register} /> 
         </aside>
 
-      </div>
+        <div>
+        <input type="submit"/>
+        </div>
+
+      {/* </div> */}
     </form>
   )
 }
