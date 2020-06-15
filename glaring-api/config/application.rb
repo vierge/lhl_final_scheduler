8ac98f431ae23d44e86c26fc2dd3127b7ad04cc1
vierge <marancholia@gmail.com>
2020-06-15 16:07:48 -0400

    installed bcrypt, rack-cors, setup

--- glaring-api/config/application.rb
+++ glaring-api/config/application.rb
@@ -24,6 +24,18 @@ module GlaringApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          expose:["Authorization"],
+          methods: [:get, :path, :put, :delete, :post, :options, :show]
+        )
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
