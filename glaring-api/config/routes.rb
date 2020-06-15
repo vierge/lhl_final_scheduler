344c6f4125928cbdb4e2c969f5acac007b2cd853
vierge <marancholia@gmail.com>
2020-06-15 16:27:09 -0400

    added devise routes for users. need 2 update docs

--- glaring-api/config/routes.rb
+++ glaring-api/config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   
 
-  devise_for :users
+  
   scope path: '/api', controller: :api do
     root to: 'api#index'
-  
-    resources :users, shallow: true do
-      get '/login', to: 'sessions#create', as: :login
-      get '/logout', to: 'sessions#destroy', as: :logout
-    end
+    
+    devise_for :users
 
     resources :groups, shallow: true do
       resources :events  
