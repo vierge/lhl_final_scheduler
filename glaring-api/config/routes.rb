86a5d10f3a80617954bea8f848fd7daa087ddc75
vierge <marancholia@gmail.com>
2020-06-15 16:28:23 -0400

    changed devise routes

--- glaring-api/config/routes.rb
+++ glaring-api/config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
   scope path: '/api', controller: :api do
     root to: 'api#index'
     
-    devise_for :users
+    devise_for :users,
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
 
     resources :groups, shallow: true do
       resources :events  
