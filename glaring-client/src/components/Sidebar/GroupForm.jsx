e93e1371a02fdd6c3e31fce70d4a431bbbb93bfc
vierge <marancholia@gmail.com>
2020-06-18 13:55:30 -0400

    alright, this is it

--- glaring-client/src/components/Sidebar/GroupForm.jsx
+++ glaring-client/src/components/Sidebar/GroupForm.jsx
@@ -68,79 +68,99 @@ export default function GroupForm(props) {
           position: absolute;
           top: 0;
           left: 0;
+          display: flex;
+          flex-direction: column;
+          align-items: left;
+          justify-content: top;
         `}
       >
-        <label htmlFor="name">NAME</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Enter group name"
-          ref={register({
-            required: "Required",
-          })}
-        />
-        {errors.name && errors.name.type === "required" && (
-          <span
-            css={css`
-              background-color: white;
-              color: red;
-              font-size: 30px;
-            `}
-          >
-            Name field cannot be empty
-          </span>
-        )}
+        <div
+          css={css`
+            height: 50%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+          `}
+        >
+          <div>
+            <label htmlFor="name">NAME</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter group name"
+              ref={register({
+                required: "Required",
+              })}
+            />
 
-        <label htmlFor="details">DETAILS</label>
-        <input
-          type="text"
-          name="description"
-          placeholder="Describe your group!"
-          ref={register({
-            required: true,
-          })}
-        />
-        {errors.description && errors.description.type === "required" && (
-          <span>Description field cannot be empty</span>
-        )}
+            {errors.name && errors.name.type === "required" && (
+              <span
+                css={css`
+                  background-color: white;
+                  color: red;
+                  font-size: 30px;
+                `}
+              >
+                Name field cannot be empty
+              </span>
+            )}
+          </div>
 
-        <label htmlFor="photo">UPLOAD PIC</label>
-        <input
-          type="url"
-          name="photo"
-          placeholder="URL of Group photo"
-          ref={register({
-            required: "Required",
-            pattern: {
-              message: "Group Photo Required",
-            },
-          })}
-        />
-        {errors.photo && errors.photo.type === "required" && (
-          <span>You must submit a group photo link</span>
-        )}
-
-        <label htmlFor="colour">CHOOSE A COLOUR</label>
-        <input
-          type="button"
-          name="colour"
-          placeholder="colour picker"
-          onClick={() => setColour({ ...colour, visible: !colour.visible })}
-        />
-        {colour.visible && (
-          <SketchPicker
-            css={css`
-              position: relative;
-              left: 100%;
-              top: 0;
-              border: none;
-              color: white;
-              background-color: black;
-            `}
-            color={colour.value}
-            onChange={(color) => setColour({ ...colour, value: color.hex })}
-          />
-        )}
+          <div>
+            <label htmlFor="details">DETAILS</label>
+            <input
+              type="text"
+              name="description"
+              placeholder="Describe your group!"
+              ref={register({
+                required: true,
+              })}
+            />
+            {errors.description && errors.description.type === "required" && (
+              <span>Description field cannot be empty</span>
+            )}
+          </div>
+          <div>
+            <label htmlFor="photo">UPLOAD PIC</label>
+            <input
+              type="url"
+              name="photo"
+              placeholder="URL of Group photo"
+              ref={register({
+                required: "Required",
+                pattern: {
+                  message: "Group Photo Required",
+                },
+              })}
+            />
+            {errors.photo && errors.photo.type === "required" && (
+              <span>You must submit a group photo link</span>
+            )}
+          </div>
+          <div>
+            <label htmlFor="colour">CHOOSE A COLOUR</label>
+            <input
+              type="button"
+              name="colour"
+              placeholder="colour picker"
+              onClick={() => setColour({ ...colour, visible: !colour.visible })}
+            />
+            {colour.visible && (
+              <SketchPicker
+                css={css`
+                  position: relative;
+                  left: 100%;
+                  top: 0;
+                  border: none;
+                  color: white;
+                  background-color: black;
+                `}
+                color={colour.value}
+                onChange={(color) => setColour({ ...colour, value: color.hex })}
+              />
+            )}
+          </div>
+        </div>
 
         <div
           css={css`
